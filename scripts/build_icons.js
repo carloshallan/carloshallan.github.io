3278f7a2a1028b9aeb380ab6873aa3691934ebdd
carloshallan <carloshallan@gmail.com>
2021-10-31 13:25:32 -0300

    add some images

--- scripts/build_icons.js
+++ scripts/build_icons.js
@@ -5,11 +5,18 @@ const mainFolder = "src/assets/";
 const iconsFolder = "src/components/icons/";
 const validExtensions = ['.svg'];
 
+function runLint() {
+    const { stdout } = execa('yarn', ['lint']);
+    execa('echo', ['vue-cli-service lint']).stdout.pipe(process.stdout);
+}
+
 console.log(
     `// Converting svg files to vue component...`
 )
+
 fs.readdir(mainFolder, (err, files) => {
    if (err) throw new Error(err);
+   const promises = [];
    console.log('Searching for files...');
    files.forEach((fileName) => {
        const filePath = `${mainFolder}${fileName}`;
@@ -22,35 +29,37 @@ fs.readdir(mainFolder, (err, files) => {
 
             fs.access(output, fs.F_OK, (err) => {
                 if (err) {
-                    fs.readFile(filePath, {encoding:'UTF8'},(err, data) => {
-                        if (err) throw new Error(err);
-                        const vueComponent = `
-                            <template>
-                                ${data}
-                            </template>
-                            <script lang="ts">
-                            import { defineComponent } from "vue";
-                        
-                            export default defineComponent({
-                                name: "${basename}"
+                    promises.push(
+                        fs.readFile(filePath, {encoding:'UTF8'},(err, data) => {
+                            if (err) throw new Error(err);
+                            const vueComponent = `
+                                <template>
+                                    ${data}
+                                </template>
+                                <script lang="ts">
+                                import { defineComponent } from "vue";
+                            
+                                export default defineComponent({
+                                    name: "${basename}"
+                                });
+                                </script>
+                            `
+                            try {
+                            fs.writeFile(output, vueComponent, 'UTF8', () => {
+                                console.log(`${output} created as successfully!`);
                             });
-                            </script>
-                        `
-                        try {
-                        fs.writeFile(output, vueComponent, 'UTF8', () => {
-                            console.log(`${output} created as successfully!`)
-                            const { stdout } = execa('yarn', ['lint']);
-                            execa('echo', ['vue-cli-service lint']).stdout.pipe(process.stdout);
-                        });
-                        } catch(err) {
-                            console.error(err);
-                        }
-                    });
+                            } catch(err) {
+                                console.error(err);
+                            }
+                        })
+                    )
+
                     return
                 }
-              
+                
                 console.log(`${basename}.vue already exists!`);
-              })
+            })
         }
-   })
+   });
+   runLint();
 });
