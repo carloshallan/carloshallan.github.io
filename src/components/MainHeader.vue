e411dea216563d140b1054ec7d186efb41e1b6d4
carloshallan <carloshallan@gmail.com>
2021-11-02 01:45:25 -0300

    add types

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -7,21 +7,28 @@
       </span>
     </div>
     <navigator />
+    <nav class="languages">
+      <span v-for="(index, locale) in languages" :key="index">{{
+        locale
+      }}</span>
+    </nav>
   </header>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive } from 'vue'
 import MainLogo from '@/components/icons/MainLogo.vue'
 import Navigator from '@/components/Navigator.vue'
 
 export default defineComponent({
   name: 'MainHeader',
   components: { MainLogo, Navigator },
-  data() {
+  setup() {
+    let currentLanguage = 'EN'
+    const languages: Array<string> = ['EN', 'PT-BR']
+
     return {
-      currentLaguage: 'EN',
-      languages: ['EN', 'PT-BR'],
+      currentLanguage,
     }
   },
 })
--- src/components/Navigator.vue
+++ src/components/Navigator.vue
@@ -2,18 +2,18 @@
   <nav>
     <router-link to="#"> test </router-link>
   </nav>
+
+  <script lang="ts">
+    import { defineComponent } from 'vue'
+
+    export default defineComponent({
+      name: 'Navigator',
+    })
+  </script>
+
+  <style lang="stylus" scoped>
+
+    nav
+    	background-color red
+  </style>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'Navigator',
-})
-</script>
-
-<style lang="stylus" scoped>
-
-nav
-	background-color red
-</style>
