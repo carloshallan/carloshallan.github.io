5a19b59ae9059a3b79313945db7b6d281cb47f8a
carloshallan <carloshallan@gmail.com>
2021-11-06 07:57:31 -0300

    add button component and icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>Comming soon...</div>
+  <div>
+    <Presentation />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Presentation from './sections/Presentation.vue'
 
 export default defineComponent({
   name: 'Home',
+  components: { Presentation },
 })
 </script>
--- src/views/sections/Presentation.vue
+++ src/views/sections/Presentation.vue
@@ -1,13 +1,30 @@
 <template>
-  <Section />
+  <Section color="transparent">
+    <figure>
+      <Profile />
+      <h1>I'm Carlos <span>Hallan</span></h1>
+      <figcaption>A Pipeline Engineer and UI/UX Designer</figcaption>
+    </figure>
+  </Section>
 </template>
 
-<script lamg="ts">
+<script lang="ts">
 import { defineComponent } from 'vue'
 import Section from '@/components/Section.vue'
+import Profile from '@/components/icons/Profile.vue'
 
 export default defineComponent({
   name: 'Presentation',
-  components: { Section },
+  components: { Section, Profile },
 })
 </script>
+<style lang="stylus">
+@import "../../styles/default"
+
+figure
+   background dark
+   display flex
+   align-items center
+   justify-content center
+   flex-direction column
+</style>
