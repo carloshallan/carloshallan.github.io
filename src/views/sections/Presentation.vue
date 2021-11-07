700cd56dbd4d2e61f900c15fda34e74d111b98fc
carloshallan <carloshallan@gmail.com>
2021-11-07 00:40:25 -0300

    update style tokens and add mouse icon

--- src/views/sections/Presentation.vue
+++ src/views/sections/Presentation.vue
@@ -2,17 +2,19 @@
   <Section color="transparent">
     <div class="presentation">
       <Profile />
-      <h1>I'm Carlos <span class="green">Hallan</span></h1>
-      <div class="subtitle">
-        A Pipeline
-        <span class="light-pink">UI/UX Designer</span>
-        and
-        <span class="light-purple">Full Stack Engineer</span>
+      <div class="titleGroup">
+        <h1>I'm Carlos <span class="green">Hallan</span></h1>
+        <div class="subtitle">
+          A Pipeline
+          <span class="light-pink">UI/UX Designer</span>
+          and
+          <span class="light-purple">Full Stack Engineer</span>
+        </div>
       </div>
       <div class="buttonGroup">
-        <Button>see my works</Button>
-        <Button outlined>about me</Button>
+        <Button>see my works</Button> <Button outlined>about me</Button>
       </div>
+      <mouse-icon />
     </div>
   </Section>
 </template>
@@ -22,19 +24,26 @@ import { defineComponent } from 'vue'
 import Section from '@/components/Section.vue'
 import Profile from '@/components/icons/Profile.vue'
 import Button from '@/components/Button.vue'
+import MouseIcon from '@/components/icons/Mouse.vue'
 
 export default defineComponent({
   name: 'Presentation',
-  components: { Section, Profile, Button },
+  components: { Section, Profile, Button, MouseIcon },
 })
 </script>
 <style lang="stylus">
 @import "../../styles/default"
 
 .presentation
+   padding: 100px 0
    background dark
    display flex
    align-items center
    justify-content center
    flex-direction column
+   gap 60px
+
+.buttonGroup
+   display flex
+   gap 20px
 </style>
