c53accc27edb079f64e6dcf3edfd6d323cfe3463
carloshallan <carloshallan@gmail.com>
2021-11-08 21:07:19 -0300

    make a default for section title border styles and create my work title

--- src/views/sections/MyWorks.vue
+++ src/views/sections/MyWorks.vue
@@ -1,5 +1,16 @@
 <template>
-  <Section color="dark-pink"> My work </Section>
+  <Section color="dark-pink">
+    <div class="row">
+      <div class="column">slide</div>
+      <div class="column">
+        <h1>
+          <span class="light-pink">!=</span>
+          What <span class="green">I</span> can do?
+        </h1>
+        <p>Lorem ipsum</p>
+      </div>
+    </div>
+  </Section>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
@@ -10,3 +21,31 @@ export default defineComponent({
   components: { Section },
 })
 </script>
+
+<style lang="stylus" scoped>
+@import "../../styles/default";
+
+h1
+  position relative
+  text-align right
+  padding-bottom 35px
+  margin-bottom 25px
+
+h1:before
+  titleBorderBottom()
+  width: 100px
+  background-color pink
+  top -25px
+  right 0
+
+h1::after
+  titleBorderBottom()
+  width: 100%
+  background-color pink
+  right 0
+  bottom 0
+  z-index 6
+
+.row
+  justify-content space-between
+</style>
