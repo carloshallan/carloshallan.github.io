86f17f5b0687a39626b400305893c194a44bec8e
carloshallan <carloshallan@gmail.com>
2021-11-12 00:58:53 -0300

    add work timeline section

---
+++ src/views/sections/WorkTimelineSection.vue
@@ -0,0 +1,14 @@
+<template>
+  <Section class="workTimelineSection"></Section>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Section from '@/components/Section.vue'
+
+export default defineComponent({
+  name: 'WorkTimelineSection',
+  components: {
+    Section
+  }
+})
+</script>
