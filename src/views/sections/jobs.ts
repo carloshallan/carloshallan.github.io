59f108880bb10c103dad0b20b620794ad3fce627
C.Hallan <carloshallan@gmail.com>
2025-01-21 20:06:15 -0300

    improve stack and toool

--- src/views/sections/jobs.ts
+++ src/views/sections/jobs.ts
@@ -10,52 +10,35 @@ export default [
       listOfStack: [
         {
           description:
-            'Python 3.9 for developing scalable backend services and automation scripts.'
+            'Python 3.11, Flask, SQLAlchemy with MySQL, Alembic, Pytest.'
+        },
+        {
+          description: 'Desktop Applications with PySide 6'
+        },
+        {
+          description: 'QT Design System for Python'
         },
         {
           description:
-            'Flask framework for building lightweight APIs and server-side logic.'
+            'Desktop applications with Python + ElectronJS for Windows, MacOs and Linux Distros.'
         },
         {
-          description:
-            'SQLAlchemy with MySQL for database modeling and management.'
-        },
-        { description: 'Alembic for database migrations.' },
-        { description: 'Pytest for writing and automating unit tests.' },
-        {
-          description:
-            'PySide 6 for creating cross-platform desktop applications with native UIs.'
+          description: 'SaaS applications with VueJS, typescript, vuetify.'
         },
         {
-          description:
-            'QT Design System to standardize Python application interfaces.'
+          description: 'Prototype with Figma.'
         },
         {
-          description:
-            'ElectronJS combined with Python for hybrid desktop apps supporting Windows, macOS, and Linux.'
+          description: 'Plugins for Autodesk Maya'
         },
         {
-          description:
-            'VueJS and Vuetify for building interactive and responsive SaaS applications.'
+          description: 'Scripts for Google Spreadsheet and Google Drive API'
         },
         {
-          description: 'Figma for rapid prototyping and collaborative design.'
+          description: 'Mailchimp Integrations'
         },
         {
-          description:
-            'Autodesk Maya plugin development to extend 3D modeling capabilities.'
-        },
-        {
-          description:
-            'Google Drive API and Spreadsheet scripting for automation and data integration.'
-        },
-        {
-          description:
-            'Firebase Realtime Database for responsive and synchronized data handling.'
-        },
-        {
-          description:
-            'Mailchimp API for marketing automation and email campaign management.'
+          description: 'Firebase realtime database.'
         }
       ]
     }
@@ -69,31 +52,13 @@ export default [
         'Developed a comprehensive CRM system for a VFX studio, leading from conceptual design to full deployment. Collaborated across teams to deliver an intuitive and robust solution tailored to studio needs.',
       title: 'Stack and Tools:',
       listOfStack: [
-        { description: 'ReactJS for dynamic and reusable UI components.' },
         {
-          description: 'TypeScript to enhance code quality and maintainability.'
+          description:
+            'ReactJS, Typescript, Redux, Redux-toolkit, Styled-Components, Chakra-UI.'
         },
         {
           description:
-            'Redux and Redux-toolkit for state management in complex applications.'
-        },
-        {
-          description:
-            'Styled-Components for styling React components with dynamic theming.'
-        },
-        {
-          description:
-            'Chakra-UI for implementing accessible and customizable design systems.'
-        },
-        { description: 'Python 3.9 for backend logic and integrations.' },
-        { description: 'Flask for API development and backend services.' },
-        {
-          description: 'SQLAlchemy with MySQL for robust database interactions.'
-        },
-        { description: 'Alembic for efficient schema migrations.' },
-        {
-          description:
-            'Pytest for testing backend services and ensuring code quality.'
+            'Python 3.9, Flask, SQLAlchemy with MySQL, Alembic, Pytest.'
         }
       ]
     }
@@ -108,24 +73,13 @@ export default [
       title: 'Stack and Tools:',
       listOfStack: [
         {
-          description:
-            'Figma for creating detailed prototypes and design workflows.'
+          description: 'Prototype with Figma.'
         },
         {
-          description:
-            'NextJS for server-side rendering and optimized web application performance.'
+          description: 'Web apps with NextJS, Typescript and ChakraUI.'
         },
         {
-          description:
-            'TypeScript for improving development scalability and debugging.'
-        },
-        {
-          description:
-            'ChakraUI for consistent and accessible component design.'
-        },
-        {
-          description:
-            'Firebase Realtime Database for real-time data synchronization.'
+          description: 'Firebase realtime database.'
         }
       ]
     }
@@ -139,25 +93,10 @@ export default [
         'Developed an After Effects extension offering streamlined project management capabilities. Managed the entire development cycle as a UX/UI Designer and Full Stack Engineer, delivering a cross-platform solution.',
       title: 'Stack and Tools:',
       listOfStack: [
+        { description: 'Prototype with Figma' },
         {
           description:
-            'Figma for prototyping UI/UX workflows and user interactions.'
-        },
-        {
-          description:
-            'ReactJS for building modular and reusable single-page applications (SPAs).'
-        },
-        {
-          description:
-            'TypeScript for ensuring type safety and code robustness.'
-        },
-        {
-          description:
-            'Vite for fast and optimized development build processes.'
-        },
-        {
-          description:
-            'Adobe ExtendScript for scripting and extending After Effects capabilities.'
+            'SPA application with ReactJS + Typescript + Vite + Adobe ExtendScript'
         }
       ]
     }
@@ -173,37 +112,46 @@ export default [
       listOfStack: [
         {
           description:
-            'Adobe XD, Sketch, Figma, and Invision Studio for prototyping and collaboration.'
+            'Prototypes with Adobe XD, Sketch, Figma and Invision Studio.'
         },
         {
           description:
-            'Vanilla JavaScript for lightweight and fast application logic.'
+            'SaaS applications with Vanilla Javascript, VueJS, ReactJs, Django and Python.'
+        },
+        {
+          description: 'Microservices with python and javascript'
         },
         {
           description:
-            'VueJS and ReactJS for building responsive and interactive SaaS applications.'
-        },
-        { description: 'Django for developing server-side web applications.' },
-        {
-          description:
-            'Microservices with Python and JavaScript for modular functionality.'
+            'Plugins and extensions for Autodesk Maya with Python and Pyside.'
         },
         {
           description:
-            'Plugins for Autodesk Maya to enhance 3D production workflows.'
-        },
-        {
-          description: 'Desktop applications using ElectronJS or Python + Qt.'
+            'Extensions for Adobe After Effects  with javascript, Vuejs.'
         },
         {
           description:
-            'PostgreSQL and MySQL for database scalability and reliability.'
+            'Desktop solutions with ElectronJs, Nodejs, Vuejs or Python + Qt'
+        },
+        {
+          description: 'PostgresSQL and MySQL with Django or Sequelize.'
+        },
+        {
+          description: 'Mixpanel integration to a Design System.'
         },
         {
           description:
-            'ShotGrid integrations to streamline production data workflows.'
+            'Shotgrid (Shotgun) integrations. Shotgrid toolkit custom scripts, and automate scripts for Shotgrid web data.'
         },
-        { description: 'Trello integrations for agile project management.' }
+        {
+          description: 'Trello integrations'
+        },
+        {
+          description: 'React Design System'
+        },
+        {
+          description: ' Qt Design System for Python'
+        }
       ]
     }
   },
@@ -216,12 +164,15 @@ export default [
         'Designed and developed a project management solution, including branding, UX prototypes, and front-end implementation, delivering an intuitive tool for creative teams.',
       title: 'Stack and Tools:',
       listOfStack: [
-        { description: 'Adobe XD for creating UX/UI prototypes.' },
         {
-          description:
-            'ReactJS for building dynamic and user-friendly SaaS applications.'
+          description: 'Prototype with Adobe XD.'
         },
-        { description: 'Python and Flask for backend API development.' }
+        {
+          description: 'SaaS application with ReactJS.'
+        },
+        {
+          description: 'Backend with python and Flask'
+        }
       ]
     }
   },
@@ -235,20 +186,16 @@ export default [
       title: 'Stack and Tools:',
       listOfStack: [
         {
-          description:
-            'Sketch and Inkscape for crafting high-quality prototypes and designs.'
+          description: 'Prototype with Sketch and Inkscape.'
         },
         {
-          description:
-            'Vanilla JavaScript for developing responsive and lightweight applications.'
+          description: 'Monolith application with vanilla javascript and PHP.'
         },
         {
-          description:
-            'PHP for building backend logic and integration with databases.'
+          description: 'Websites with vanilla javascript and PHP.'
         },
         {
-          description:
-            'WordPress templates and APIs for CMS-based web development.'
+          description: 'Wordpress templates and API.'
         }
       ]
     }
@@ -259,20 +206,23 @@ export default [
     date: '2 years and 9 months (full time)',
     jobDescription: {
       description:
-        'Managed digital marketing campaigns and web development projects, focusing on WordPress websites, team coordination, and optimizing online presence for increased visibility.',
+        'I started as a designer but quickly moved up, managing digital marketing campaigns and web development projects. This included focusing on WordPress websites, coordinating teams, and enhancing online visibility for improved reach.',
       title: 'Stack and Tools:',
       listOfStack: [
         {
-          description: 'WordPress for creating and managing CMS-based websites.'
+          description: 'Websites with vanilla javascript.'
         },
         {
-          description:
-            'Google Analytics for tracking and optimizing web traffic.'
+          description: 'Wordpress templates.'
         },
-        { description: 'Facebook Ads for targeted marketing campaigns.' },
         {
-          description:
-            'Social media design and content creation for brand promotion.'
+          description: 'Google Analytics.'
+        },
+        {
+          description: 'Facebook ADS.'
+        },
+        {
+          description: 'Designs for Social Media'
         }
       ]
     }
@@ -286,12 +236,15 @@ export default [
         'Provided technical support, customer training, and database management, ensuring seamless operation of Delphi and web-based software solutions.',
       title: 'Stack and Tools:',
       listOfStack: [
-        { description: 'Support for Delphi-based software solutions.' },
         {
-          description:
-            'PHP and Vanilla JavaScript for maintaining web applications.'
+          description: 'Support for Delphi Softwares.'
         },
-        { description: 'MySQL for database management and optimization.' }
+        {
+          description: 'PHP and Vanilla javascript softwares.'
+        },
+        {
+          description: 'MySql Database.'
+        }
       ]
     }
   }
