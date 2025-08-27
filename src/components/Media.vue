ce21dcbb836e03731c6485ade992c5bfbcbba48d
C.Hallan <carloshallan@gmail.com>
2025-08-27 10:30:22 -0300

    add desktop apps

--- src/components/Media.vue
+++ src/components/Media.vue
@@ -2,6 +2,19 @@
   <div class="Media">
     <v-img v-if="isImage" :src="src" max-width="100%" class="media"></v-img>
 
+    <iframe
+      v-else-if="isUrl"
+      class="media"
+      width="100%"
+      height="592"
+      :src="src"
+      :title="alt"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerpolicy="strict-origin-when-cross-origin"
+      allowfullscreen
+    ></iframe>
+
     <v-responsive v-else max-width="100%">
       <video controls width="100%" class="media" :src="src"></video>
     </v-responsive>
@@ -21,6 +34,9 @@ export default Vue.extend({
   computed: {
     isImage() {
       return /\.(jpeg|jpg|gif|png)$/i.test(this.src)
+    },
+    isUrl() {
+      return /^(https?:\/\/|www\.)/.test(this.src)
     }
   }
 })
